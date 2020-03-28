c837dc3743689bf533fcaf8707da2e4e7d6f0d33
BuildTools <unconfigured@null.spigotmc.org>
2020-03-28 17:06:13 +0100

    Added numbers and language selection to the app

--- README.md
+++ README.md
@@ -4,6 +4,13 @@ This little Python script will help you with creating passphrases in your own la
 
 But since this is still in environment and not really productive at the moment, I don't recommend using it, until I have a few more language and everything working as it should. :)
 
-At the moment only german is available and with a not really curated passphrase list.
+At this moment, the following languages are available:
+* [Catalan](https://github.com/Jautenim/diceware-cat) - [CC-BY-4.0](https://creativecommons.org/licenses/by/4.0/)
+* [Czech](http://world.std.com/~reinhold/diceware_wordlist_cz.txt) - [GNU General Public License](http://www.gnu.org/licenses/gpl-3.0.html)
+* [Danish](http://pastebin.com/rFatW8up) - [CC-BY-4.0](https://creativecommons.org/licenses/by/4.0/)
+* [German](http://world.std.com/%7Ereinhold/diceware_german.txt) - [GNU General Public License](http://www.gnu.org/licenses/gpl-3.0.html)
+* [English](http://world.std.com/%7Ereinhold/diceware.wordlist.asc) - [CC-BY-4.0](https://creativecommons.org/licenses/by/4.0/)
+* [Esperanto](http://esperantajxo.blogspot.com/2014/07/dajsvaro.html)
+* [Spanish](http://world.std.com/%7Ereinhold/diceware_en_espanolA.htm)
 
 This is a project for my #100DaysOfCode Challenge. If you're interested in what this is, [you should go to the website https://www.100daysofcode.com/](https://www.100daysofcode.com/)
--- passphrase.py
+++ passphrase.py
@@ -1,22 +1,43 @@
 #!/bin/python
 import argparse
 import random
+import os
+import gettext
+from pathlib import Path
+
+# generate needed variables and functions to translate my app.
+localedir = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'locales')
+translate_de = gettext.translation('passphrase', localedir, fallback=True, languages=['de-DE'])
+translate_en = gettext.translation('passphrase', localedir, fallback=True, languages=['en-US'])
+translate_en.install()
+_ = translate_en.gettext
 
 
 def passphrase_generator(argv):
     # open the correct language
-    language_file = 'languages/{}.txt'.format(argv.language)
+    data_folder = Path('languages/')
+    language_file = data_folder / '{}.txt'.format(argv.language)
     with open(language_file) as passphrases:
         inputs = passphrases.readlines()  # load the language file into a list
         # get random values from that list
         passphrase = random.sample(inputs, argv.count)
         # strip the list from any \n characters
         passphrase = [i.replace('\n', '') for i in passphrase]
-        # should the output be case sensitive or insensitive?
-        if argv.case_sensitive:
+        # should the output be capitalized?
+        if argv.capitalize:
             passphrase = [word.capitalize() for word in passphrase]
-        elif argv.case_insensitive:
-            passphrase = [word.lower() for word in passphrase]
+        # if the user wants numbers in his passphrase, we add them here
+        if argv.numbers:
+            if argv.count <= 3:
+                min_count = 1
+                max_count = argv.count
+            else:
+                min_count = 2
+                max_count = random.randint(3, argv.count)
+            number_items = random.sample(range(0,argv.count), random.randint(min_count,max_count))
+            for item in number_items:
+                change_word = passphrase[item] + str(random.randrange(9))
+                passphrase[item] = change_word
         # convert the list to a string and add a delimiter.
         passphrase = argv.delimiter.join(passphrase)
         print(passphrase)
@@ -25,17 +46,14 @@ def passphrase_generator(argv):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("-d", "--delimiter", default=' ',
-                        help="Definiere das Trennzeichen (Standard: Leertaste)")
+                        help=_("Define a delimiter for your passphrase (Standard: space)"))
     parser.add_argument("-c", "--count", type=int,
-                        default=4, help="Anzahl der Wörter")
-    case_group = parser.add_mutually_exclusive_group()
-    case_group.add_argument("-cs", "--case_sensitive", action="store_true",
-                            help="Großschreibung aller Wörter aktivieren")
-    case_group.add_argument("-ci", "--case_insensitive", action="store_true",
-                            help="Kleinschreibung aller Wörter aktivieren")
+                        default=4, help=_("How many words should your passphrase have (Standard: 4)"))
+    parser.add_argument("-ca", "--capitalize", action="store_true",
+                        help=_("Activate capitalizing all words"))
     parser.add_argument("-n", "--numbers", action="store_true",
-                        help="Füge Nummern im Passphrase hinzu")
-    parser.add_argument("-l", "--language", action="store_true", default="de",
-                        help="Sprache des Passphrases (Standard: Deutsch)")
+                        help=_("Add numbers to your passphrase"))
+    parser.add_argument("-l", "--language", default="en",
+                        help=_("Language of your passphrase (Standard: englisch)"))
     args = parser.parse_args()
     passphrase_generator(args)
